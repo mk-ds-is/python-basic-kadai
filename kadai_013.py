0ee75a4cd65dffb1f1a7d812f61495b9d96a0587
mk-ds-is <328.maryan@gmail.com>
2024-10-27 15:25:07 +0900

    Add files via upload

--- kadai_013.py
+++ kadai_013.py
@@ -1,6 +1,5 @@
 def calculate_total(price, tax):
     tax_amount = price * (tax / 100)
     total_price = price + tax_amount
-    print(f"{total_price}円")
-
+    return total_price
 calculate_total(100, 10)
